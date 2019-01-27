78c516cd4f7523441fbe97e5bf4aa7dfd4cbb80d
Aleksei Besogonov <alex.besogonov@gmail.com>
2019-01-27 05:02:30 -0800

    Added the formula

---
+++ Homebrew Formula/aurora-apollo-client.rb
@@ -0,0 +1,34 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://www.rubydoc.info/github/Homebrew/brew/master/Formula
+class AuroraApolloClient < Formula
+  desc "Apollo task scheduler"
+  homepage "https://github.com/aurorasolar/apollo"
+  url "https://github.com/Cyberax/homebrew-tools/raw/master/bins/apollo-release-v0.0.1.tar.gz"
+  sha256 "f4f222391b1f697ded46b6bc8506a07759b9e8760a67b269d60db0146a043da7"
+#  head "git://git@github.com:aurorasolar/apollo.git", :branch=> "master"
+  depends_on "go" => :build
+  depends_on "dep" => :build
+  conflicts_with "apollo", :because => "Also has apollo binary"
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure	 
+    system "bash gobuild.sh"
+    system "bash -c '.gobuild/src/apollo/apollo completion > .gobuild/src/apollo/apollo_completion'"
+    bin.install ".gobuild/src/apollo/apollo" => "apollo"
+    bash_completion.install ".gobuild/src/apollo/apollo_completion"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test apollo-release-v`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "apollo --help"
+  end
+end
